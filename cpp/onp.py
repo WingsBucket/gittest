43e514f622bb7779ee1e86879bc31a3614bfcde4
WingsBucket <jakub.bielecki98@gmail.com>
2017-03-21 13:31:35 +0100

    =doopa

---
+++ cpp/onp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  onp.py
+#
+
+
+def main(args):
+    stos = []
+    onp = raw_input("Podaj wyrażenie: ")
+    operand = ''
+    for znak in onp:
+        if znak !=" " and znak.isdigit():
+            operand += znak
+        elif znak == " " and len(operand):
+            stos.append(float(operand))
+            operand = ""
+        elif znak in ('*', '-', '+', '/', '%'):
+            a = str(stos.pop())
+            b = str(stos.pop())
+            stos.append(eval(b+znak+a))
+
+    print "Wynik: ", stos.pop()
+    #TODO:
+    #wykrywanie błędów w zapisie ONP
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
